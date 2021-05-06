6389cb054997992183f07b485783bbcc1658eecd
Brugen-git <74241208+Brugen-git@users.noreply.github.com>
2021-05-07 01:50:05 +0530

    Update settings.py

--- Nurturelabs/settings.py
+++ Nurturelabs/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--2#(mg9p53s1&7*2qsbbo2nx8v@9zs3ry7z#0284x+s+fo*tc2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','https://nurture-labs-assignment-bhavya.herokuapp.com/']
 
